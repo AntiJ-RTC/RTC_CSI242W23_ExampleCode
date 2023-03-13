449d3baa4a3f090b369a440633aced14d494463f
AntiJ-RTC <103210593+AntiJ-RTC@users.noreply.github.com>
2023-03-13 11:41:13 -0700

    Week11

---
+++ Week11/1WebSockets.js
@@ -0,0 +1,59 @@
+let conenctionStatus = document.getElementById('connectionStatus');
+let btnConnect = document.getElementById('btnConnect');
+let btnDisconnect = document.getElementById('btnDisconnect');
+let btnSend = document.getElementById('btnSend');
+let message = document.getElementById('message');
+let response = document.getElementById('response');
+conenctionStatus.innerHTML = "Disconnected";
+
+//Websockets are a protocol that allows for real-tme communication between a client and a server
+//Before websockets, applications used HTTP to communicate. HTTp only allows for one way communication
+//With websockets, both the client and the server can send messages to each other at any time
+//Websockets are particularly useful for applications that require real-time updates:
+//Chat apps, Multiplayer games, stock market ticker
+
+//Websockets work by establishing a persistent connection between the client and the server. This means that once the connection is established, messages can be sent back and forth without the need for a new request/response cycle
+
+let webSocket = null;
+
+btnConnect.addEventListener('click', function(){
+    //assign our webSocket
+    webSocket = new WebSocket('wss://echo.websocket.events');
+
+    //set the onopen()
+    webSocket.onopen = function(){
+        conenctionStatus.innerHTML = 'Connected';
+    }
+    //onclose()
+    webSocket.onclose = function(){
+        conenctionStatus.innerHTML = "Disconnected";
+    }
+    //onmessage() we receive new information from the server
+    webSocket.onmessage = function(e){
+        response.innerHTML = e.data;
+    }
+    //onerror
+    webSocket.onerror = function(e){
+        console.log(e);
+    }
+    console.log(webSocket);
+    // 0 - Connecting - Socket is created, connection isn't open yet
+    // 1 - Open - Connection is ready for use
+    // 2 - Closing - Connection is in process of closing
+    // 3 - Closed - Connection is closed or could not be opened
+});
+btnDisconnect.addEventListener('click', function(){
+    webSocket.close();
+    conenctionStatus.innerHTML = 'Disconnected';
+});
+
+btnSend.addEventListener('click', function(){
+    // console.log(`Testing ${message.value}`);
+    //before we send lets check to make sure the connection is open
+    if(webSocket === null || webSocket.readyState !== 1){
+        alert('Connect before you send');
+    }
+    else{
+        webSocket.send(message.value);
+    }
+});
